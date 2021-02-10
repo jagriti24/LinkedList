080f23257767697856729288d883631b309337f2
jagriti24 <jagriti.singh.2407@gmail.com>
2021-02-10 23:57:27 +0530

    Create MergeTwoListsProblem.java

---
+++ MergeTwoListsProblem.java
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+// Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+ 
+
+// Example 1:
+
+
+// Input: l1 = [1,2,4], l2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: l1 = [], l2 = []
+// Output: []
+// Example 3:
+
+// Input: l1 = [], l2 = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both l1 and l2 are sorted in non-decreasing order.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MergeTwoListsProblem {
+    ListNode l3 = new ListNode();
+    ListNode start = l3;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        while(l1!=null || l2!=null){
+            if(l1==null){
+                l3.next=l2;
+                break;
+            }
+            else if(l2==null){
+                l3.next=l1;
+                break;
+            }
+            else if(l1.val<=l2.val){
+                l3.next=l1;
+                l1=l1.next;
+            }
+            else if(l1.val>l2.val){
+                l3.next=l2;
+                l2=l2.next;
+            }
+            l3 =l3.next;
+        }
+        return start.next;
+    }
+}
